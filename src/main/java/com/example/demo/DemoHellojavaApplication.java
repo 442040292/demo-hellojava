8ff89165dac6b9c2f21752a44a2d35f2ea29d936
shixiaole <shi442040292@qq.com>
2019-09-22 21:13:22 +0800

    提交 生成包和引用包

--- src/main/java/com/example/demo/DemoHellojavaApplication.java
+++ src/main/java/com/example/demo/DemoHellojavaApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoHellojavaApplication {
+public class DemoHellojavaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoHellojavaApplication.class, args);
 	}
 
+	/**
+	 * 新增此方法
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(DemoHellojavaApplication.class);
+
+	}
 }
